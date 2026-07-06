51a3bd420d8df69afe03cc37cfbe3609681c71ba
Reuben <reuben.ingber@gmail.com>
2026-07-06 10:22:58 -0400

    feat: build sessions/pricing section

---
+++ src/data/sessionTypes.js
@@ -0,0 +1,27 @@
+export const sessionTypes = [
+  {
+    name: 'Mini Session',
+    price: '$200',
+    duration: '30-minute session',
+    featured: false,
+    features: [
+      'Single location',
+      '15 edited digital images',
+      'Online gallery delivered in 5 days',
+    ],
+    note: 'Great for a quick seasonal update',
+  },
+  {
+    name: 'Standard Session',
+    price: '$300',
+    duration: '60-minute session',
+    featured: true,
+    features: [
+      'One location',
+      '25 edited digital images',
+      'Print release included',
+      'Online gallery delivered in 5 days',
+    ],
+    note: null,
+  },
+];
